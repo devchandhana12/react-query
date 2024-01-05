8bf03a8611f001ee9485d1791ab56f298a827dab
devchandhana12 <chandana.n@ascent-hr.com>
2024-01-05 11:28:30 +0530

    ***

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-400 text-3xl font-bold">
+        we are going to start our react query process
+      </h1>
+    </div>
+  );
+}
+
+export default App;
